b4259da1a78de743ccc29dcbac97706fb96e27e2
clifforddonk <clifforddonk@gmail.com>
2025-02-20 03:46:56 +0000

    new changes

---
+++ src/com/Clifford/NumberGuess.java
@@ -0,0 +1,20 @@
+package com.Clifford;
+
+import java.util.Scanner;
+import java.util.Random;
+public class NumberGuess {
+
+    public static void numberGuess() {
+
+
+        int randomNum,userGuess;
+Scanner scanner=new Scanner(System.in);
+Random rand=new Random();
+randomNum=rand.nextInt(5);
+
+
+
+
+        System.out.println("");
+    }
+}
